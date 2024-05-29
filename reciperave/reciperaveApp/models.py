c1ccb08386b173ea55b9eb4195dccd2fd7b71618
Bineesh S <sbineesh172@gmail.com>
2024-05-29 16:27:20 +0530

    Added new methods in the program

--- reciperave/reciperaveApp/models.py
+++ reciperave/reciperaveApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class upload(models.Model):
+    recipe_name = models.CharField(max_length=50)
+    total_time = models.IntegerField()
+    dishType = models.CharField(max_length=20)
+    description = models.CharField(max_length=255)
+    photo_video = models.FileField(upload_to='picture')
+    instruction = models.CharFiels(max_length=1500)
+    class Meta:
+        db_table = "upload"
